548a9e291dfbccb1d90e9b8203df463b139611f4
hychul <hychome@gmail.com>
2021-01-10 02:38:12 +0900

    Add rotary source to main keyboard sketch

--- Main/Rotary.ino
+++ Main/Rotary.ino
@@ -7,6 +7,15 @@
  * ----------------------------------------------------------------------------
  */
 
+#define ROTARTY_PIN_CLK 2 // Connected pin number to CLK on KY-040
+#define ROTARTY_PIN_DT 3 // Connected pin numbder to DT on KY-040
+#define ROTARTY_PIN_SW 4 // Connected pin numbder to SW on KY-040
+#define ROTARTY_BTN_RATE_MS 100 // Mininum rotary button recognition milli second
+
+int lastRotaryVal;
+boolean isClockwise;
+int encoderPosCount = 0;
+
 /*
  * +----------------------+
  * |        Rotary        |
@@ -14,11 +23,38 @@
  */
 
 void setupRotary() {
-  // TODO: setup for rotary
+  pinMode(ROTARTY_PIN_CLK,INPUT);
+  pinMode(ROTARTY_PIN_DT,INPUT);
+  
+  lastRotaryVal = digitalRead(ROTARTY_PIN_CLK);
 }
 
 void updateRotary() {
-  // TODO: update rotary
+  int val = digitalRead(ROTARTY_PIN_CLK);
+
+  if (val == lastRotaryVal) {
+    return;
+  }
+  
+  if (digitalRead(ROTARTY_PIN_DT) != val) {
+    isClockwise = true;
+    encoderPosCount ++;
+  } else {
+    isClockwise = false;
+    encoderPosCount--;
+  }
+  
+  Serial.print("Rotated: ");
+  if (isClockwise) {
+    Serial.println("clockwise");
+  } else {
+    Serial.println("counterclockwise");
+  }
+  
+  Serial.print("Encoder Position: ");
+  Serial.println(encoderPosCount);
+  
+  lastRotaryVal = val;
 }
 
 /*
@@ -27,10 +63,33 @@ void updateRotary() {
  * +----------------------+
  */
 
+int lastRotaryButtonVal;
+unsigned long lastRotaryButtonMs = 0;
+
 void setupRotaryButton() {
-  // TODO: setup for rotary button
+  pinMode(ROTARTY_PIN_SW,INPUT);
+  
+  lastRotaryButtonVal = digitalRead(ROTARTY_PIN_SW);
 }
 
 void updateRotaryButton() {
-  // TODO: update rotary button
+  int val = digitalRead(ROTARTY_PIN_SW);
+
+  if (val == lastRotaryButtonVal) {
+    return;
+  }
+
+  if (millis() < lastRotaryButtonMs + ROTARTY_BTN_RATE_MS) {
+    return;
+  }
+
+  Serial.print("Switch: ");
+  if (val == LOW) {
+    Serial.println("pushed");
+  } else {
+    Serial.println("released");
+  }
+
+  lastRotaryButtonVal = val;
+  lastRotaryButtonMs = millis();
 }
